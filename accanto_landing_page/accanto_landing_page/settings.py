10548a41f38d72f3ea10635ca379ec4ca25c63e2
Lucas Hernandes Andreoni <lucasandreoni2000@gmail.com>
2025-10-14 13:35:31 -0300

    localhost settings

--- accanto_landing_page/accanto_landing_page/settings.py
+++ accanto_landing_page/accanto_landing_page/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv("SECRET_KEY", default='')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'accantoinvestimentos.com.br', 'www.accantoinvestimentos.com.br']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.vercel.app', 'accantoinvestimentos.com.br', 'www.accantoinvestimentos.com.br']
 
 
 # Application definition
