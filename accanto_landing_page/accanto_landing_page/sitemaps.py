54681993c81d69782e1416ecd91cd2377ad30eed
Lucas Hernandes Andreoni <lucasandreoni2000@gmail.com>
2025-11-02 12:47:13 -0300

    configuração do sitemap

---
+++ accanto_landing_page/accanto_landing_page/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+
+class StaticViewSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+
+    def items(self):
+        # nomes das rotas definidas em landing/urls.py
+        return ['index', 'nossa_equipe', 'politica_de_privacidade']
+
+    def location(self, item):
+        return reverse(item)
